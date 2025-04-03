8487dfcf3609c353390f00470576a79764e85e51
salahmed-ctrlz <medkoursalaheddine@gmail.com>
2025-04-03 13:42:10 +0100

    Updates

--- assets/chevron-down-mn1jaLv5.js
+++ assets/chevron-down-mn1jaLv5.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-fYmHnkfp.js";/**
+import{c as o}from"./index-B4HtWXoi.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
